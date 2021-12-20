fb7527a236341927d6ae383c0454253d2d68e30c
João Vitor Sanches <joaovitormanolo@gmail.com>
2021-12-20 04:07:17 -0300

    Tree logic partially implemented

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 class myNode
 {
-    constructor(id, data)
+    constructor(id, data, ref)
     {
         this.id = [] + id;
         this.data = data;
         this.connections = [];
+        this.circle = new fabric.Circle({
+            radius: r,
+            strokeWidth: 5,
+            fill: 'transparent',
+            stroke: 'red',
+            selectable: false,
+            top: root.getCenterPoint().y,
+            left: root.getCenterPoint().x + (r + len + r),
+        });
+        this.path = new fabric.Path(`M 0 0 L ${len} 0 z`)
+        this.path.set({
+            stroke: 'green',
+            strokeWidth: 5,
+            selectable: false,
+            angle: 0,
+            top: root.getCenterPoint().y,
+            left: root.getCenterPoint().x + (r + len/2),
+        });
+        this.angle = 0;
+        this.ref = ref;
     }
 
     addNode(data)
     {
         var connect = this.connections;
         var n = 10*this.id + connect.length;
-        connect.push(new myNode(n, data));
+        connect.push(new myNode(n, data, this.circle));
     }
 
     removeNode(id)
@@ -21,6 +41,25 @@ class myNode
         } else {
             this.connections[id.shift()].removeNode(id);
         }
+    }
+
+    renderNode(){
+        nc.add(this.circle, this.path)
+        nc.renderAll()
+    }
+
+    setAngle(angle){
+        this.angle += angle
+        this.path.set({
+            angle: -this.angle,
+            top: this.ref.getCenterPoint().y - Math.sin((Math.PI*this.angle)/180)*(r + len/2),
+            left: this.ref.getCenterPoint().x + Math.cos((Math.PI*this.angle)/180)*(r + len/2),
+        });
+        this.circle.set({
+            top: this.ref.getCenterPoint().y - Math.sin((Math.PI*this.angle)/180)*(r + len + r),
+            left: this.ref.getCenterPoint().x + Math.cos((Math.PI*this.angle)/180)*(r + len + r),
+        });
+        nc.renderAll();
     }
 }
 
@@ -58,9 +97,52 @@ var testezera = (inputius) => {
     return a;
 }
 
-var shush = new myNode(0, 0);
+let r = 20;
+let len = 50;
+
+var nc = new fabric.Canvas('canvas');
+nc.set({
+    hoverCursor: 'select',
+    backgroundColor: '#bbb'
+})
+nc.setDimensions({
+    height: 500,
+    width: 500
+});
+fabric.Object.prototype.originX = 'center';
+fabric.Object.prototype.originY = 'center';
+
+var root = new fabric.Circle({
+    radius: r,
+    strokeWidth: 5,
+    fill: 'transparent',
+    stroke: 'red',
+    selectable: false
+});
+nc.add(root);
+nc.centerObject(root);
+root.setCoords();
+nc.renderAll();
+
+var setangle = (glurb, ref, node) => {
+    
+}
+
+var shush = new myNode(0, 0, root);
 shush.addNode(1);
 shush.addNode(2);
 shush.connections[0].addNode(1);
-var snesh = document.getElementById("test");
-snesh.innerHTML = testezera(shush);
+// var snesh = document.getElementById("test");
+// snesh.innerHTML = testezera(shush);
+
+shush.renderNode();
+shush.connections[0].setAngle(45);
+shush.connections[1].setAngle(-45);
+shush.connections[0].renderNode();
+shush.connections[1].renderNode();
+
+var teste = () => {
+    shush.setAngle(10);
+    shush.connections[0].setAngle(10);
+    shush.connections[1].setAngle(10);
+}
